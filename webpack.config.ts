52b8ef46416ae3beba20268dfbf00d81afcb6ed0
Michael Randolph <mrand01@gmail.com>
2019-03-13 14:50:36 -0400

    Add css-loader to webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -17,6 +17,18 @@ const config: webpack.Configuration = {
         test: /\.ts$/,
         use: 'ts-loader',
       },
+      {
+        exclude: /node_modules/,
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
     ],
   },
   output: {
